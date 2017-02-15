86a9cd77f1ce9eb97d6011d3cee4aa44ccbf9bc1
shgcx1234 <shgcx1234@gmail.com>
2017-02-15 15:40:56 +0800

    update links.sh

--- links.sh
+++ links.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-#Create ssh keys,after this paste .ssh/id_rsa.pub content to github
+#1.Create ssh keys,after this paste .ssh/id_rsa.pub content to github
 #ssh-keygen -t rsa -C shgcx1234@gmail.com
 #git clone git@github.com:shgcx1234/linux-configure-files.git ~/linux-configure-files
 
+#2.sudo linux-configure-files/links.sh
 echo "create links of configs"
 apt-get install tmux
 ln -s ~/linux-configure-files/.vimrc ~/.vimrc
@@ -15,7 +16,7 @@ ln -s ~/linux-configure-files/.vimperatorrc ~/.vimperatorrc
 git config --global user.name shgcx1234
 git config --global user.email shgcx1234@gmail.com
 
-#after this , open vim ,then :PluginInstall
+#3.after this , open vim ,then :PluginInstall
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
 #install zsh
@@ -25,8 +26,10 @@ git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
 cp ~/.oh-my-zsh/templates/zshrc.zsh-template ~/.zshrc
 
 
-#add below command into .bashrc
+#4.add below command into .bashrc
 #create or reattach a tmux session when open a term
+#if you want to pass ctrl-A to remote OregonUbuntu's tmux, you should press ctrl-A twice
+#ctrl-A ctrl-A d you can restore your last work around when you ssh to remote ubuntu
 #tmux attach-session -t "$USER" || tmux new-session -s "$USER"
 #exit
 
