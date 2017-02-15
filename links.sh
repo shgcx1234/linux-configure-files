d4225524fb4610dcf7f69630a96357cd7c2f792d
shgcx1234 <shgcx1234@gmail.com>
2017-02-15 14:58:55 +0800

    update links.sh

--- links.sh
+++ links.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
+
+#Create ssh keys,after this paste .ssh/id_rsa.pub content to github
+#ssh-keygen -t rsa -C shgcx1234@gmail.com
+#git clone git@github.com:shgcx1234/linux-configure-files.git ~/linux-configure-files
+
 echo "create links of configs"
+apt-get install tmux
 ln -s ~/linux-configure-files/.vimrc ~/.vimrc
 ln -s ~/linux-configure-files/.tmux.conf ~/.tmux.conf
 ln -s ~/linux-configure-files/.vimperatorrc ~/.vimperatorrc
@@ -14,9 +20,13 @@ git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
 #install zsh
 apt-get install zsh
-chsh -s /bin/zsh
+#chsh -s /bin/zsh #leave the default term bash and use zsh in tmux
 git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
 cp ~/.oh-my-zsh/templates/zshrc.zsh-template ~/.zshrc
 
-#Create ssh keys,after this paste .ssh/id_rsa.pub content to github
-ssh-keygen -t rsa -C shgcx1234@gmail.com
+
+#add below command into .bashrc
+#create or reattach a tmux session when open a term
+#tmux attach-session -t "$USER" || tmux new-session -s "$USER"
+#exit
+
